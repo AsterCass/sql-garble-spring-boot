abcf5c807ea9c39e49403ba855f22ed8bbc7407c
astercasc <astercas@qq.com>
2022-08-16 18:13:23 +0800

    test:

--- sql-garble-spring-boot-autoconfigure/src/main/java/com/github/aster/plugin/garble/config/GarbleConfig.java
+++ sql-garble-spring-boot-autoconfigure/src/main/java/com/github/aster/plugin/garble/config/GarbleConfig.java
@@ -4,7 +4,10 @@ import com.github.aster.plugin.garble.util.BeanMapUtil;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 配置项需要有默认值，作为 properties put 的时候，当 value 为 null 时会报一个不好定位的空指针异常
