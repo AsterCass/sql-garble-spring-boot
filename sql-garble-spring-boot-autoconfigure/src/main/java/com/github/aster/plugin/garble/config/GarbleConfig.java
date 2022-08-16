f8f4e8851b8cc4c344dfc8ab35c339f60a38a8bd
astercasc <astercas@qq.com>
2022-08-16 19:38:20 +0800

    doc:

--- sql-garble-spring-boot-autoconfigure/src/main/java/com/github/aster/plugin/garble/config/GarbleConfig.java
+++ sql-garble-spring-boot-autoconfigure/src/main/java/com/github/aster/plugin/garble/config/GarbleConfig.java
@@ -1,5 +1,6 @@
 package com.github.aster.plugin.garble.config;
 
+import com.github.aster.plugin.garble.enums.GarbleFunctionEnum;
 import com.github.aster.plugin.garble.util.BeanMapUtil;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,13 +23,31 @@ public class GarbleConfig {
         return BeanMapUtil.beanToMap(this, GarbleConfig.class);
     }
 
+    /**
+     * 监控表和监控返回字段的Map，一般为主键，("user", "id")
+     */
     private HashMap<String, String> monitoredTableMap = new HashMap<>();
 
+    /**
+     * 监控表和更新标记字段Map ("user", "update_record")
+     */
     private HashMap<String, String> monitoredTableUpdateFlagColMap = new HashMap<String, String>();
 
+    /**
+     * 默认更新标记字段，如果监控表无法在更新标记字段Map中取得，则会使用默认更新标记字段
+     */
     private String defaultFlagColName = "";
 
+    /**
+     * 不拦截的sql的路径
+     */
     private List<String> excludedMapperPath = new ArrayList<>();
+
+    /**
+     * 需要的功能
+     * {@link GarbleFunctionEnum}
+     */
+    List<Integer> garbleFunctionList;
 
 
 
