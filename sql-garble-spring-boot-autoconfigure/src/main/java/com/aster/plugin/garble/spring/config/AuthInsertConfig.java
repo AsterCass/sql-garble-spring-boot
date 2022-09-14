00259dd491ba7d88591b72d63a4bb669e42049ac
astercasc <astercas@qq.com>
2022-09-14 12:09:34 +0800

    add: 支持交集鉴权

--- sql-garble-spring-boot-autoconfigure/src/main/java/com/aster/plugin/garble/spring/config/AuthInsertConfig.java
+++ sql-garble-spring-boot-autoconfigure/src/main/java/com/aster/plugin/garble/spring/config/AuthInsertConfig.java
@@ -4,6 +4,8 @@ import com.aster.plugin.garble.spring.util.BeanMapUtil;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,27 +25,27 @@ public class AuthInsertConfig {
     /**
      * 标记实现AuthenticationCodeInterface接口的方法路径，加快加快初始化速度，可以不赋值
      */
-    private String authCodePath;
+    private String authCodePath = "";
 
     /**
      * 监控表列表
      */
-    private List<String> monitoredTableList;
+    private List<String> monitoredTableList = new ArrayList<>();
 
     /**
      * 监控表和权限标记列
      */
-    private Map<String, String> monitoredTableAuthColMap;
+    private Map<String, String> monitoredTableAuthColMap = new HashMap<>();
 
     /**
      * 监控表的默认权限标记列，当monitoredTableUpdateFlagColMap无法查询到需要监控表的权限标记列的时候，使用默认权限标记列
      */
-    private String defaultAuthColName;
+    private String defaultAuthColName = "";
 
     /**
      * 在此map中的的sql不受到监控，即使包含监控表
      */
-    private List<String> excludedMapperPath;
+    private List<String> excludedMapperPath = new ArrayList<>();
 
 
 }
--- sql-garble-spring-boot-autoconfigure/src/main/java/com/aster/plugin/garble/spring/config/GarbleConfig.java
+++ sql-garble-spring-boot-autoconfigure/src/main/java/com/aster/plugin/garble/spring/config/GarbleConfig.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 /**
  * 配置项需要有默认值，作为 properties put 的时候，当 value 为 null 时会报一个不好定位的空指针异常
+ * 我们允许配置项不填写
  * @author astercasc
  */
 @Data
